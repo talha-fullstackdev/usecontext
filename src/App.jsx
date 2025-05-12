2d3d46bf0f4c5517912ba50a26abeec993855c37
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-12 16:25:15 +0500

    import app.css file in react component to use scss , wwe have to use css file in which scss is compiled

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,8 @@ const App = () => {
    {/* <ComA/> */}
    {/* <Api/> */}
    {/* <Example/> */}
-   <Rotation/>
+   {/* <Rotation/> */}
+   <Sass/>
     </div>
   );
 };
--- src/scss/Sass.jsx
+++ src/scss/Sass.jsx
@@ -1,25 +1,23 @@
-
-import useTitle from '../UseTitle'
-import "./app.scss"
+import useTitle from "../UseTitle";
+import "./app.css";
 const Sass = () => {
-  useTitle("scss tutorial")
+  useTitle("scss tutorial");
   return (
     <>
       <div className="main_div">
-      <h1 className='heading'>Talha Nawaz</h1>
-      <h1 className="para">24 years old</h1>
-      <h1 className="para">from abbottabad</h1>
-      <h1 className="para">lives in lahore</h1>
-      <button className='btn1'>click me</button>
-      <button className='btn2' >click me</button>
-    </div>
-    <h1 className='import_color'>imported variables from another file</h1>
-    <p className="paragraph1">mix color using mixing function</p>
-    <p className="paragraph2">mix color using mixing function</p>
-    <p className="paragraph3">mix color using mixing function</p>
+        <h1 className="heading">Talha Nawaz</h1>
+        <h1 className="para">24 years old</h1>
+        <h1 className="para">from abbottabad</h1>
+        <h1 className="para">lives in lahore</h1>
+        <button className="btn1">click me</button>
+        <button className="btn2">click me</button>
+      </div>
+      <h1 className="import_color">imported variables from another file</h1>
+      <p className="paragraph1">mix color using mixing function</p>
+      <p className="paragraph2">mix color using mixing function</p>
+      <p className="paragraph3">mix color using mixing function</p>
     </>
-  
-  )
-}
+  );
+};
 
-export default Sass
+export default Sass;
