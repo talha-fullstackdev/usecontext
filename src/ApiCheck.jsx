0a93503a67d4ec9bacd18e8a92d20964d265e2ff
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-12 15:47:50 +0500

    updated

--- src/ApiCheck.jsx
+++ src/ApiCheck.jsx
@@ -1,6 +1,5 @@
 import  { useState, useEffect } from 'react';
 import useTitle from './UseTitle';
-
 const ApiCheck = () => {
   useTitle("fetch data from backend")
   const [apiData, setApiData] = useState([]);
