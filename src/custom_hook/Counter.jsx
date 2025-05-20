77b8c731b8cfc8dc39d4b53dcc82f7623f0fa757
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-20 14:09:36 +0500

    formated code

--- src/custom_hook/Counter.jsx
+++ src/custom_hook/Counter.jsx
@@ -1,11 +1,10 @@
-
 import useCount from './useCount'
 import useTitle from '../UseTitle'
 import chalk from 'chalk'
 const Counter = () => {
+  useTitle("counter")
   let user = true;
     user ? console.log(chalk.green("present")):console.log(chalk.red("not prseent"))
-    useTitle("counter")
     const[count, increment, decrement, reset] = useCount()
   return (
     <div>
